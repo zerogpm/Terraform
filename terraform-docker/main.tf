5d9b37752114f047ab1d223c8b7b318e65805d6b
zerogpm <zerogpm@gmail.com>
2022-01-04 11:35:06 -0800

    add image module

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.15.0"
-    }
-  }
-}
-
-provider "docker" {}
-
 resource "null_resource" "dockervol" {
   provisioner "local-exec" {
     command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
@@ -19,14 +8,14 @@ resource "random_uuid" "random" {
   count = local.container_count
 }
 
-resource "docker_image" "nodered_image" {
-  name = var.image[terraform.workspace]
+module "image" {
+  source = "./image"
 }
 
 resource "docker_container" "nodered_container" {
   count = local.container_count
   name  = join("-", ["nodered", terraform.workspace, random_uuid.random[count.index].result])
-  image = docker_image.nodered_image.latest
+  image = module.image.image_out
   ports {
     internal = 1880
     external = var.external_port[terraform.workspace][count.index]
