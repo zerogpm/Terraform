a89786af3614dcd4b9b89c91022c2aecfbe7f435
zerogpm <zerogpm@gmail.com>
2022-01-06 17:53:32 -0800

    using for each

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,22 +1,29 @@
+locals {
+  deployment = {
+    nodered = {
+      image = var.image["nodered"][terraform.workspace]
+    }
+    influxdb = {
+      image = var.image["influxdb"][terraform.workspace]
+    }
+  }
+}
+
 resource "random_uuid" "random" {
   count = local.container_count
 }
 
-module "nodered_image" {
+module "image" {
   source = "./image"
-  image_in = var.image["nodered"][terraform.workspace]
-}
-
-module "influxdb_image" {
-  source = "./image"
-  image_in = var.image["influxdb"][terraform.workspace]
+  for_each = local.deployment
+  image_in = each.value.image
 }
 
 module "container" {
   source = "./container"
   count = local.container_count
   name_in = join("-", ["nodered", terraform.workspace, random_uuid.random[count.index].result])
-  image_in = module.nodered_image.image_out
+  image_in = module.image["nodered"].image_out
   int_port_in = 1880
   ext_port_in = var.external_port[terraform.workspace][count.index]
   container_path_in = "/data"
