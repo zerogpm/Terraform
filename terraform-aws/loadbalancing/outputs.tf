4959dd03dd32bf0ed4868c3888903a0ec7576a16
zerogpm <zerogpm@gmail.com>
2022-01-22 14:39:20 -0800

    add some output for ALB endpoint

--- terraform-aws/loadbalancing/outputs.tf
+++ terraform-aws/loadbalancing/outputs.tf
@@ -2,3 +2,7 @@
 output "lb_target_group_arn" {
   value = aws_lb_target_group.bu-tg.arn
 }
+
+output "lb_endpoint" {
+  value = aws_lb.bu-alb.dns_name
+}
---
+++ terraform-aws/outputs.tf
@@ -0,0 +1,4 @@
+#--- root/outputs.tf ---
+output "load_balancer_endpoint" {
+  value = module.loadbalancing.lb_endpoint
+}
