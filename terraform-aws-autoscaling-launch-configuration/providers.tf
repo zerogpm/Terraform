077b6be1d368ca99aafd2b467877eff3a7363b2d
zerogpm <zerogpm@gmail.com>
2022-02-13 19:23:11 -0800

    add autoscaling module

---
+++ terraform-aws-autoscaling-launch-configuration/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region                  = var.aws_region
+  shared_credentials_file = var.aws_region
+  profile                 = var.profile
+}
