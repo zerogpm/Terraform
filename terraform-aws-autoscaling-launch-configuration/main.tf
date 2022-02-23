b4e189d8467f321ac486a6db86bc5168a66d08a2
zerogpm <zerogpm@gmail.com>
2022-02-22 22:02:22 -0800

    add bastion host

--- terraform-aws-autoscaling-launch-configuration/main.tf
+++ terraform-aws-autoscaling-launch-configuration/main.tf
@@ -5,3 +5,7 @@ module "custom-vpc" {
   private_subnets = [for i in range(101, 103, 1) : cidrsubnet("10.0.0.0/16", 8, i)]
   security_groups = local.security_group
 }
+
+module "bastion" {
+  source = "./b"
+}
