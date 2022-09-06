88680028c2d1ee0073919f37dc0df330c493cbea
zerogpm <zerogpm@gmail.com>
2022-09-05 18:26:30 -0700

    mutiple provider

---
+++ terraform-azure/mutiple-provider-configuration/versions.tf
@@ -0,0 +1,23 @@
+# Terraform Block
+terraform {
+  required_version = ">= 0.15"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+    features {
+      virtual_machine {
+        delete_os_disk_on_deletion = false # This will ensure when the Virtual Machine is destroyed, Disk is not deleted, default is true and we can alter it at provider level
+      }
+    }
+    alias = "provider2-westus"
+}
