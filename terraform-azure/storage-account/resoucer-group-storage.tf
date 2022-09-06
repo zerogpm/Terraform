02117be8b06dcf4d9f0a3ed75f348146a6fd3d82
zerogpm <zerogpm@gmail.com>
2022-09-06 12:49:35 -0700

    add storage

---
+++ terraform-azure/storage-account/resoucer-group-storage.tf
@@ -0,0 +1,25 @@
+# Resource-1: Azure Resource Group
+resource "azurerm_resource_group" "myrg1" {
+  name = "myrg-1"
+  location = "Canada Central"
+}
+
+# Resource-2: Random String 
+resource "random_string" "myrandom" {
+  length = 16
+  upper = false 
+  special = false
+}
+
+# Resource-3: Azure Storage Account 
+resource "azurerm_storage_account" "mysa" {
+  name                     = "mysa${random_string.myrandom.id}"
+  resource_group_name      = azurerm_resource_group.myrg1.name
+  location                 = azurerm_resource_group.myrg1.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
