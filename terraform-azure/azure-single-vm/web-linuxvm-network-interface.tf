907e5ee81e9434e18c99efcb90692ffd2c190b8d
zerogpm <zerogpm@gmail.com>
2023-06-10 08:16:13 -0700

    add

--- terraform-azure/azure-single-vm/web-linuxvm-network-interface.tf
+++ terraform-azure/azure-single-vm/web-linuxvm-network-interface.tf
@@ -11,3 +11,29 @@ resource "azurerm_network_interface" "web_linuxvm_nic" {
     public_ip_address_id          = azurerm_public_ip.web_linuxvm_publicip.id
   }
 }
+
+resource "azurerm_network_interface" "web_linuxvm_nic_2" {
+  name                = "${local.resource_name_prefix}-web-linuxvm-nic-2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "web-linuxvm-ip-1"
+    subnet_id                     = azurerm_subnet.websubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.web_linuxvm_publicip_2.id
+  }
+}
+
+# resource "azurerm_network_interface" "web_linuxvm_nic_3" {
+#   name                = "${local.resource_name_prefix}-web-linuxvm-nic-3"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+
+#   ip_configuration {
+#     name                          = "web-linuxvm-ip-3"
+#     subnet_id                     = azurerm_subnet.websubnet.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.web_linuxvm_publicip_3.id
+#   }
+# }
