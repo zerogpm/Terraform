d8d39ab68fd836ac5d2b85dff8568cd8dfc7f0b3
zerogpm <zerogpm@gmail.com>
2023-06-12 10:36:20 -0700

    lists map

---
+++ terraform-aws-2023/Terraform-Loops-MetaArgument-Count-For-loops-Lists-Maps/utility-project/ec2instance.tf
@@ -0,0 +1,20 @@
+# Resource: EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami = data.aws_ami.amzlinux2.id
+  user_data = templatefile("${path.root}/userdata.tpl", {})
+  instance_type = var.instance_type
+  #instance_type = var.instance_type_list[1]  # For List
+  #nstance_type = var.instance_type_map["prod"]  # For Map
+  key_name = var.instance_keypair
+  vpc_security_group_ids = [
+    aws_security_group.vpc-ssh.id,
+    aws_security_group.vpc-web.id
+  ]
+  #for_each = toset(data.aws_availability_zones.my_azones.names)
+  for_each = toset(keys({for az, details in data.aws_ec2_instance_type_offerings.my_ins_type: 
+    az => details.instance_types if length(details.instance_types) != 0 }))
+  availability_zone = each.key # You can also use each.value because for list items each.key == each.value
+  tags = {
+    "Name" = "For-Each-Demo-${each.key}"
+  }
+}
---
+++ terraform-aws-2023/Terraform-Loops-MetaArgument-Count-For-loops-Lists-Maps/utility-project/version.tf
@@ -0,0 +1,18 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.2.0" # Optional but recommended in production
+    }
+  }
+} 
+# Provider Block
+provider "aws" {
+  region = var.aws_region
+}
+
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+$HOME/.aws/credentials
+*/
