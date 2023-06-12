91e78a4a7316e64cca28cab9769815750ba0804a
zerogpm <zerogpm@gmail.com>
2023-06-11 20:41:42 -0700

    for each

---
+++ terraform-aws-2023/Terraform-Loops-MetaArgument-Count-For-loops-Lists-Maps/For-each/ec2instance.tf
@@ -0,0 +1,27 @@
+# Availability Zones Datasource
+data "aws_availability_zones" "my_azones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+# Resource: EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami = data.aws_ami.amzlinux2.id
+  user_data = templatefile("${path.root}/userdata.tpl", {})
+  instance_type = var.instance_type
+  #instance_type = var.instance_type_list[1]  # For List
+  #nstance_type = var.instance_type_map["prod"]  # For Map
+  key_name = var.instance_keypair
+  vpc_security_group_ids = [
+    aws_security_group.vpc-ssh.id,
+    aws_security_group.vpc-web.id
+  ]
+  # Create EC2 Instance in all Availabilty Zones of a VPC  
+  for_each = toset(data.aws_availability_zones.my_azones.names)
+  availability_zone = each.key  # You can also use each.value because for list items each.key == each.value
+  tags = {
+    "Name" = "Count-Demo-${each.value}"
+  }
+}
