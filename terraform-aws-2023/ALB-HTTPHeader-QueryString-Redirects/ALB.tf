7fbda9f22ade72a00f6bd2198959f980c4e80789
zerogpm <zerogpm@gmail.com>
2023-06-16 16:44:04 -0700

    fix issues

--- terraform-aws-2023/ALB-HTTPHeader-QueryString-Redirects/ALB.tf
+++ terraform-aws-2023/ALB-HTTPHeader-QueryString-Redirects/ALB.tf
@@ -89,31 +89,32 @@ module "alb" {
           port      = 80
         }
       }
-      tags =local.common_tags # Target Group Tags
-    }  
+      tags = local.common_tags # Target Group Tags
+    }
   ]
 
   # HTTPS Listener
   https_listeners = [
     # HTTPS Listener Index = 0 for HTTPS 443
     {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = module.acm.acm_certificate_arn
-      action_type = "fixed-response"
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = module.acm.acm_certificate_arn
+      action_type     = "fixed-response"
       fixed_response = {
         content_type = "text/plain"
         message_body = "Fixed Static message - for Root Context"
         status_code  = "200"
       }
-    }, 
+    },
   ]
 
   # HTTPS Listener Rules
   https_listener_rules = [
-    # Rule-1: /app1* should go to App1 EC2 Instances
-    { 
+    # Rule-1: custom-header=my-app-1 should go to App1 EC2 Instances
+    {
       https_listener_index = 0
+      priority             = 1
       actions = [
         {
           type               = "forward"
@@ -121,12 +122,16 @@ module "alb" {
         }
       ]
       conditions = [{
-        host_headers = [var.app1_dns_name]
+        http_headers = [{
+          http_header_name = "custom-header"
+          values           = ["app-1", "app1", "my-app-1"]
+        }]
       }]
     },
     # Rule-2: /app2* should go to App2 EC2 Instances    
     {
       https_listener_index = 0
+      priority             = 2
       actions = [
         {
           type               = "forward"
@@ -134,9 +139,47 @@ module "alb" {
         }
       ]
       conditions = [{
-        host_headers = [var.app2_dns_name]
+        http_headers = [{
+          http_header_name = "custom-header"
+          values           = ["app-2", "app2", "my-app-2"]
+        }]
       }]
-    },    
+    },
+    # Rule-3: When Query-String, ?google=gg redirect to https://google.com
+    {
+      https_listener_index = 0
+      priority             = 3
+      actions = [{
+        type        = "redirect"
+        status_code = "HTTP_302"
+        host        = "google.com"
+        path        = "/"
+        query       = ""
+        protocol    = "HTTPS"
+      }]
+      conditions = [{
+        query_strings = [{
+          key   = "google"
+          value = "gg"
+        }]
+      }]
+    },
+    # Rule-4: When Host Header = app1.devopeasyway.com, redirect to https://www.binaryunicornsoftware.com/
+    {
+      https_listener_index = 0
+      priority             = 4
+      actions = [{
+        type        = "redirect"
+        status_code = "HTTP_302"
+        host        = "www.binaryunicornsoftware.com"
+        path        = "/"
+        query       = ""
+        protocol    = "HTTPS"
+      }]
+      conditions = [{
+        host_headers = [var.app1_dns_name]
+      }]
+    }
   ]
 
   tags = local.common_tags # ALB Tags
