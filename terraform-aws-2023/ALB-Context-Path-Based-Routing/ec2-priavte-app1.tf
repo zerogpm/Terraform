931243256a47844a52419495168f3f927c5e9dcf
zerogpm <zerogpm@gmail.com>
2023-06-15 22:37:29 -0700

    ALB-Contex-Path

---
+++ terraform-aws-2023/ALB-Context-Path-Based-Routing/ec2-priavte-app1.tf
@@ -0,0 +1,22 @@
+# AWS EC2 Instance Terraform Module
+# Bastion Host - EC2 Instance that will be created in VPC Public Subnet
+module "ec2_private_app1" {
+  depends_on = [
+    module.vpc
+  ]
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.1.0"
+  #for_each = toset([ module.vpc.private_subnets[0],module.vpc.private_subnets[1] ])
+  for_each  = toset(["0", "1"])
+  subnet_id = element(module.vpc.private_subnets, tonumber(each.key))
+  # insert the 10 required variables here
+  name          = "${var.environment}-vm-app1"
+  ami           = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+  key_name      = var.instance_keypair
+  #monitoring             = true
+
+  vpc_security_group_ids = [module.private_sg.security_group_id]
+  user_data              = templatefile("${path.root}/userdata-1.tpl", {})
+  tags                   = local.common_tags
+}
