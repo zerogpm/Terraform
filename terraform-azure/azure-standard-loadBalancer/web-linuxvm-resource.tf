465b99ad16cec828c57f0e56b329532fb40bf40e
zerogpm <zerogpm@gmail.com>
2023-09-02 19:19:09 -0700

    add user-data

--- terraform-azure/azure-standard-loadBalancer/web-linuxvm-resource.tf
+++ terraform-azure/azure-standard-loadBalancer/web-linuxvm-resource.tf
@@ -1,24 +1,3 @@
-# Locals Block for custom data
-locals {
-webvm_custom_data = <<CUSTOM_DATA
-#!/bin/sh
-#sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl enable httpd
-sudo systemctl start httpd  
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
-sudo chmod -R 777 /var/www/html 
-sudo echo "Welcome to chris - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
-sudo mkdir /var/www/html/app1
-sudo echo "Welcome to chris - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
-sudo echo "Welcome to chris - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Chris - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
-sudo curl -H "Metadata:true" --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" -o /var/www/html/app1/metadata.html
-CUSTOM_DATA
-}
-
-
 # Resource: Azure Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "web_linuxvm" {
   count = var.web_linuxvm_instance_count
@@ -45,5 +24,5 @@ resource "azurerm_linux_virtual_machine" "web_linuxvm" {
     version = "20.04.202106030"
   }  
   #custom_data = filebase64("${path.module}/app-scripts/redhat-webvm-script.sh")
-  custom_data = base64encode(local.webvm_custom_data)
+  custom_data = filebase64("user-data.tpl")
 }
