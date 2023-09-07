ff85215f5bf352b8918258b060f8a611c32c8909
zerogpm <zerogpm@gmail.com>
2023-09-06 17:21:09 -0700

    fmt

--- terraform-azure/VM-scale-sets/web-linuxvm-vmss-resource.tf
+++ terraform-azure/VM-scale-sets/web-linuxvm-vmss-resource.tf
@@ -2,11 +2,11 @@
 resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
   name = "${local.resource_name_prefix}-web-vmss"
   #computer_name = "web-linux-vm" # Hostname of the VM (Optional)
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  sku                   = "Standard_DS1_v2"
-  instances             = 2
-  admin_username        = "azureuser"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard_DS1_v2"
+  instances           = 2
+  admin_username      = "azureuser"
 
   admin_ssh_key {
     username   = "azureuser"
@@ -28,13 +28,13 @@ resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
   upgrade_mode = "Automatic"
 
   network_interface {
-    name = "web-vmss-nic"
-    primary = "true"
+    name                      = "web-vmss-nic"
+    primary                   = "true"
     network_security_group_id = azurerm_network_security_group.web_vmss_nsg.id
     ip_configuration {
-      name = "internal"
-      primary = true
-      subnet_id = azurerm_subnet.websubnet.id
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.websubnet.id
       load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id]
     }
   }
