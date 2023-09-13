393ce004554ae26ebb620e507790f63fe15534bd
Chris Su <zerogpm@gmail.com>
2023-09-12 22:01:42 -0700

    add app vmss

--- terraform-azure/internal-lb-with-vmss/web-app-vmss-resource.tf
+++ terraform-azure/internal-lb-with-vmss/web-app-vmss-resource.tf
@@ -1,20 +1,20 @@
 locals {
-  webvm_custom_data = <<CUSTOM_DATA
+  app_custom_data = <<CUSTOM_DATA
 #!/bin/sh
 #sudo apt update
 sudo apt install nginx -y
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash && wait
-sudo az storage blob download -c ${azurerm_storage_container.httpd_files_container.name} -f /etc/nginx/sites-available/frontend.conf -n frontend.conf --account-name ${azurerm_storage_account.storage_account.name} --account-key ${azurerm_storage_account.storage_account.primary_access_key}
+sudo az storage blob download -c ${azurerm_storage_container.httpd_files_container.name} -f /etc/nginx/sites-available/backend.conf -n backend.conf --account-name ${azurerm_storage_account.storage_account.name} --account-key ${azurerm_storage_account.storage_account.primary_access_key}
 sudo rm /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-available/default
-sudo ln -s /etc/nginx/sites-available/frontend.conf /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/backend.conf /etc/nginx/sites-enabled/
 sudo systemctl reload nginx
 CUSTOM_DATA  
 }
 
 # Resource: Azure Linux Virtual Machine
-resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
-  name = "${local.resource_name_prefix}-web-vmss"
+resource "azurerm_linux_virtual_machine_scale_set" "app_vmss" {
+  name = "${local.resource_name_prefix}-app-vmss"
   #computer_name = "web-linux-vm" # Hostname of the VM (Optional)
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -37,19 +37,19 @@ resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
     version   = "22.04.202309030"
   }
   #custom_data = filebase64("${path.module}/app-scripts/redhat-webvm-script.sh")
-  custom_data = base64encode(local.webvm_custom_data)
+  custom_data = base64encode(local.app_custom_data)
 
   upgrade_mode = "Automatic"
 
   network_interface {
-    name                      = "web-vmss-nic"
+    name                      = "app-vmss-nic"
     primary                   = "true"
-    network_security_group_id = azurerm_network_security_group.web_vmss_nsg.id
+    network_security_group_id = azurerm_network_security_group.app_vmss_nsg.id
     ip_configuration {
       name                                   = "internal"
       primary                                = true
       subnet_id                              = azurerm_subnet.websubnet.id
-      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id]
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.app_lb_backend_address_pool.id]
     }
   }
 
--- terraform-azure/internal-lb-with-vmss/web-linux-vmss-autoscaling-default-profile.tf
+++ terraform-azure/internal-lb-with-vmss/web-linux-vmss-autoscaling-default-profile.tf
@@ -19,7 +19,7 @@ Resource: azurerm_monitor_autoscale_setting
     2. Scale-Up Rule: Decrease VMs by 1 when LB SYN Count is less than 10 Connections (Average)    
 */
 
-/* resource "azurerm_monitor_autoscale_setting" "web_vmss_autoscale" {
+ resource "azurerm_monitor_autoscale_setting" "web_vmss_autoscale" {
   name                = "${local.resource_name_prefix}-web-vmss-autoscale-profiles"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -29,7 +29,7 @@ Resource: azurerm_monitor_autoscale_setting
     email {
       send_to_subscription_administrator    = true
       send_to_subscription_co_administrator = true
-      custom_emails                         = []
+      custom_emails                         = [var.web_monitor_autoscale_setting_email]
     }
   }
   ################################################################################
@@ -180,5 +180,5 @@ Resource: azurerm_monitor_autoscale_setting
   } # End of Profile-1
 
 
-} */
+}
 
